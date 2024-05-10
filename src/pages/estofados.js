9b56d7cf79264b49a4e3fbe416ab529810a73d6e
AAndreazza <alanaandreazza26@hotmail.com>
2024-05-10 11:01:13 -0300

    making it valid to netlify

---
+++ src/pages/estofados.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Estofados = () => {
+    return (
+        <div className="bg-white">
+            <h1>Em andamento</h1>
+        </div>
+    )
+}
+
+export default Estofados
